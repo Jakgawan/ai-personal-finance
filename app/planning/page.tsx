a3ae492d10b021b2a423fa89a4d5e5f70270d9d3
jakgawan <jagkawan2000@gmail.com>
2026-03-30 21:57:02 +0700

    add Dashboard page

--- app/planning/page.tsx
+++ app/planning/page.tsx
@@ -1,7 +1,11 @@
-export default function Planning (){
-    return(
-        <div>
-            Planning
-        </div>
-    )
+export default function Dashboard(){
+  return (
+ <main className="p-8">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+     <div className="bg-white rounded-lg p-6 shadow">
+      <p>Total</p>
+      <p>฿ 0</p>
+     </div>
+  </main>
+  )
 }
