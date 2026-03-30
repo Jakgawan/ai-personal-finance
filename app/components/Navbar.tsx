ad7a1cffc038b15ebc2c7c5024cd058a9c322c36
jakgawan <jagkawan2000@gmail.com>
2026-03-30 21:04:46 +0700

    add Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,12 +2,12 @@ import Link  from "next/link";
 
 export default function Navbar() {
     return (
-<nav>
-    <Link href="/">Dashboard</Link>
-    <Link href="/transaction">Transaction</Link>
-    <Link href="/planning">Planning</Link>
-    <Link href="/business">Business</Link>
-    <Link href="/ai">AI</Link>
+<nav className="flex gap-4 bg-gray-500 p-4">
+    <Link href="/" className="text-white hover:text-gray-300">Dashboard</Link>
+    <Link href="/transaction" className="text-white hover:text-gray-300">Transaction</Link>
+    <Link href="/planning" className="text-white hover:text-gray-300">Planning</Link>
+    <Link href="/business" className="text-white hover:text-gray-300">Business</Link>
+    <Link href="/ai" className="text-white hover:text-gray-300">AI</Link>
 </nav>
     )
 }
