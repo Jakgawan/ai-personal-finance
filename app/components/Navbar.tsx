86d59154c55cfd6e65b73d08710fb95e9918d1c5
jakgawan <jagkawan2000@gmail.com>
2026-03-30 21:26:42 +0700

    add active state to Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,16 @@
+"use client"
+import { usePathname } from "next/navigation";
 import Link  from "next/link";
 
 export default function Navbar() {
+    const pathname = usePathname()
     return (
 <nav className="flex gap-4 bg-gray-500 p-4">
-    <Link href="/" className="text-white hover:text-gray-300">Dashboard</Link>
-    <Link href="/transaction" className="text-white hover:text-gray-300">Transaction</Link>
-    <Link href="/planning" className="text-white hover:text-gray-300">Planning</Link>
-    <Link href="/business" className="text-white hover:text-gray-300">Business</Link>
-    <Link href="/ai" className="text-white hover:text-gray-300">AI</Link>
+    <Link href="/" className={pathname === "/" ? "text-white font-bold" : "text-gray-300"}>Dashboard</Link>
+    <Link href="/transaction" className={pathname === "/transaction" ? "text-white font-bold" : "text-gray-300"}>Transaction</Link>
+    <Link href="/planning" className={pathname === "/planning" ? "text-white font-bold" : "text-gray-300"}>Planning</Link>
+    <Link href="/business" className={pathname === "/business" ? "text-white font-bold" : "text-gray-300"}>Business</Link>
+    <Link href="/ai" className={pathname === "/ai" ? "text-white font-bold" : "text-gray-300"}>AI</Link>
 </nav>
     )
 }
